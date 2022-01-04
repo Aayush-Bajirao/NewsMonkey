dbda6f8bc8656f542793758de53eef13c55712d8
Aayush Bajirao <aayubaji1208@gmail.com>
2022-01-04 12:31:37 +0530

    Added navbar and donee some changes in index files

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
 
 export default class App extends Component {
   render() {
-    c = 'jhon';
     return (
       <div>
-        Hello my first class based app react app!!!{this.c}
+
+        <Navbar />
+
       </div>
     )
   }
