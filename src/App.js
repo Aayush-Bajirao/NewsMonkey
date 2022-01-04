8131b63622a417d28e229c89b0646ff9703835d3
Aayush Bajirao <aayubaji1208@gmail.com>
2022-01-04 13:23:40 +0530

    Added some new components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
+import News from './components/News';
 
 export default class App extends Component {
   render() {
@@ -10,6 +11,8 @@ export default class App extends Component {
       <div>
 
         <Navbar />
+
+        <News />
 
       </div>
     )
