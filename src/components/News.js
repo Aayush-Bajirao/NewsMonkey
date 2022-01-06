9a2d488613b51f2f72b2bb65a5afe586ed0ad889
Aayush Bajirao <aayubaji1208@gmail.com>
2022-01-06 10:09:28 +0530

    Added multiple cards and passed props

--- src/components/News.js
+++ src/components/News.js
@@ -1,18 +1,35 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class News extends Component {
-    render() {
-        return (
-            <div>
-                
-                News Component
-                <NewsItem />
-                <NewsItem />
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container my-3">
+        <h2>NewsMonkey Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-3">
+            <NewsItem
+              title="My Title"
+              description="this is the description of the following news about this web page."
+            />
+          </div>
+          <div className="col-md-3">
+            <NewsItem
+              title="My Title"
+              description="this is the description of the following news about this web page."
+            />
+          </div>
+          <div className="col-md-3">
+            <NewsItem
+              title="My Title"
+              description="this is the description of the following news about this web page."
+            />
+          </div>
+          
+        </div>
+      </div>
+    );
+  }
 }
 
-export default News
+export default News;
