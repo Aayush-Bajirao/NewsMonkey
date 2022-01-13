904d5fbf75e2c42244dc9932d8abed2e73359908
Aayush Bajirao <aayubaji1208@gmail.com>
2022-01-13 10:48:04 +0530

    Used dame function for ComponentDidMount ,next and previous

--- src/App.js
+++ src/App.js
@@ -20,14 +20,14 @@ export default class App extends Component {
           
 
         <Routes>
-          <Route exact path="/" element={<News pageSize={9} country= 'in' category='general' apiKey='2492d27b405f4e4ba6b250bed0a238a2'/>}></Route>
-          <Route exact path="/business" element={<News key="business" pageSize={9} country= 'in' category='business' apiKey='2492d27b405f4e4ba6b250bed0a238a2'/>}></Route>   
-          <Route exact path="/entertainment" element={<News key="entertainment" pageSize={9} country= 'in' category='entertainment' apiKey='2492d27b405f4e4ba6b250bed0a238a2'/>}></Route>
-          <Route exact path="/general" element={<News key="general" pageSize={9} country= 'in' category='general' apiKey='2492d27b405f4e4ba6b250bed0a238a2'/>}></Route>   
-          <Route exact path="/health" element={<News key="health" pageSize={9} country= 'in' category='health' apiKey='2492d27b405f4e4ba6b250bed0a238a2'/>}></Route>
-          <Route exact path="/science" element={<News key="science" pageSize={9} country= 'in' category='science' apiKey='2492d27b405f4e4ba6b250bed0a238a2'/>}></Route> 
-          <Route exact path="/sports" element={<News key="sports" pageSize={9} country= 'in' category='sports' apiKey='2492d27b405f4e4ba6b250bed0a238a2'/>}></Route>
-          <Route exact path="/technology" element={<News key="technology" pageSize={9} country= 'in' category='technology' apiKey='2492d27b405f4e4ba6b250bed0a238a2'/>}></Route>               
+          <Route exact path="/" element={<News pageSize={9} country= 'in' category='general'  />}></Route>
+          <Route exact path="/business" element={<News key="business" pageSize={9} country= 'in' category='business'  />}></Route>   
+          <Route exact path="/entertainment" element={<News key="entertainment" pageSize={9} country= 'in' category='entertainment'  />}></Route>
+          <Route exact path="/general" element={<News key="general" pageSize={9} country= 'in' category='general'  />}></Route>   
+          <Route exact path="/health" element={<News key="health" pageSize={9} country= 'in' category='health'  />}></Route>
+          <Route exact path="/science" element={<News key="science" pageSize={9} country= 'in' category='science'  />}></Route> 
+          <Route exact path="/sports" element={<News key="sports" pageSize={9} country= 'in' category='sports'  />}></Route>
+          <Route exact path="/technology" element={<News key="technology" pageSize={9} country= 'in' category='technology'  />}></Route>               
         </Routes>
 
         </Router>
--- src/components/News.js
+++ src/components/News.js
@@ -1,22 +1,20 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
-import PropTypes from 'prop-types'
-
+import PropTypes from "prop-types";
 
 export class News extends Component {
-
   static defaultPops = {
-    country: 'in',
+    country: "in",
     pageSize: 9,
     category: "general",
-  }
+  };
 
   static propTypes = {
     country: PropTypes.string,
     pageSize: PropTypes.number,
     category: PropTypes.string,
-  }
+  };
 
   constructor() {
     super();
@@ -29,50 +27,29 @@ export class News extends Component {
       /*pageSize: 15  my doing */
     };
   }
-
+  apiKey = "2492d27b405f4e4ba6b250bed0a238a2";
   async componentDidMount() {
     console.log("cdm");
 
-    let data = await fetch(
-      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=1&pageSize=${this.props.pageSize}`
+   /* let data = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.apiKey}&page=1&pageSize=${this.props.pageSize}`
     );
     this.setState({ loading: true });
     let parsedData = await data.json();
-    /*console.log(parsedData)*/
+    /*console.log(parsedData)
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
       loading: false,
-    });
+    });*/
+    this.updateNews();
   }
 
-  handleNextClick = async () => {
-    console.log("Next");
-    /*Primary plan to disable the button */
-    if (
-      !Math.ceil(this.state.totalResults / this.props.pageSize) <
-      this.state.page + 1
-    ) {
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${
-        this.state.page + 1
-      }&pageSize=${this.props.pageSize}`;
-      this.setState({ loading: true });
-      let data = await fetch(url);
-      let parsedData = await data.json();
-      /*console.log(parsedData)*/
-      this.setState({
-        articles: parsedData.articles,
-        page: this.state.page + 1,
-        loading: false,
-      });
-    }
-  };
-
-  handlePrevClick = async () => {
-    console.log("Prevous");
-
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${
-      this.state.page - 1
+  async updateNews(){
+    let url = `https://newsapi.org/v2/top-headlines?country=${
+      this.props.country
+    }&category=${this.props.category}&apiKey=${this.apiKey}&page=${
+      this.state.page
     }&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
@@ -80,32 +57,81 @@ export class News extends Component {
     /*console.log(parsedData)*/
     this.setState({
       articles: parsedData.articles,
-      page: this.state.page - 1,
+      totalResults: parsedData.totalResults,
       loading: false,
     });
+  };
+
+  handleNextClick = async () => {
+    /*console.log("Next");
+    Primary plan to disable the button
+    let url = `https://newsapi.org/v2/top-headlines?country=${
+      this.props.country
+    }&category=${this.props.category}&apiKey=${this.apiKey}&page=${
+      this.state.page + 1
+    }&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData)
+    this.setState({
+      articles: parsedData.articles,
+      page: this.state.page + 1,
+      loading: false,
+    });*/
+    this.setState({
+      page: this.state.page + 1
+    });
+    this.updateNews();
+  };
+
+  handlePrevClick = async () => {
+    /*console.log("Prevous");
+
+    let url = `https://newsapi.org/v2/top-headlines?country=${
+      this.props.country
+    }&category=${this.props.category}&apiKey=${this.apiKey}&page=${
+      this.state.page - 1
+    }&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    /*console.log(parsedData)
+    this.setState({
+      articles: parsedData.articles,
+      page: this.state.page - 1,
+      loading: false,
+    });*/
+    this.setState({  
+      page: this.state.page - 1, 
+    });
+    this.updateNews();
   };
 
   render() {
     return (
       <div className="container my-3">
-        <h1 className="text-center" style={{margin: '35px 0'}}>NewsMonkey Top Headlines</h1>
+        <h1 className="text-center" style={{ margin: "35px 0" }}>
+          NewsMonkey Top Headlines
+        </h1>
         {this.state.loading && <Spinner />}
         <div className="row my-4">
-          {(!this.state.loading) && this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItem
-                  source={element.source.name}
-                  author={element.author}
-                  date={element.publishedAt}
-                  title={element.title}
-                  description={element.description}
-                  imgUrl={element.urlToImage}
-                  newsUrl={element.url}
-                />
-              </div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    source={element.source.name}
+                    author={element.author}
+                    date={element.publishedAt}
+                    title={element.title}
+                    description={element.description}
+                    imgUrl={element.urlToImage}
+                    newsUrl={element.url}
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="container d-flex justify-content-between">
           <button
